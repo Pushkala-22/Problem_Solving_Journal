e4f3e0f1ecf9199b239d92c4716f2745c19fb7a6
Pushkala-22 <pushkalachelluri@gmail.com>
2025-08-16 13:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (22.99%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int diffBits = start ^ goal;
+      
+        // This variable will hold the count of the number of flips required.
+        int flipCount = 0;
+      
+        // Process each bit of 'diffBits' to count the number of set bits (flips required).
+        while (diffBits != 0) {
+            // Increment 'flipCount' if the least significant bit of 'diffBits' is 1.
+            flipCount += diffBits & 1;
+          
+            // Right shift 'diffBits' by 1 to process the next bit.
+            diffBits >>= 1;
+        }
+      
+        // Return the count of flips required.
+        return flipCount;
+    }
+}
