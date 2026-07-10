36d1658551c0aafd5f3835a11921ff96272615c8
Pushkala-22 <pushkalachelluri@gmail.com>
2026-07-10 14:38:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (76.33%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int a=cost[0];
+        int b=cost[1];
+        int c=0;
+        for(int i=2;i<cost.length;i++){
+             c=cost[i]+(Math.min(a,b));
+            a=b;
+            b=c;
+        }
+        return Math.min(a,b);
+    }
+}
