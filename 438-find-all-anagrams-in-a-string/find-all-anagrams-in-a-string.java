e313401b7bad45ac2e18dae96d8c9bdfe36f843c
Pushkala-22 <pushkalachelluri@gmail.com>
2026-07-17 23:17:08 +0530

    Time: 11 ms (65.91%) | Memory: 46.7 MB (72.49%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,21 +1,29 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
-        int n = s.length(), k = p.length();
-        if (n < k) return result;
+        if (s.length() < p.length()) return result;
 
         int[] pCount = new int[26];
+        int[] sCount = new int[26];
+
+        // frequency of pattern
         for (char c : p.toCharArray()) {
             pCount[c - 'a']++;
         }
 
-        for (int i = 0; i <= n - k; i++) {
-            int[] sCount = new int[26];
-            for (int j = i; j < i + k; j++) {
-                sCount[s.charAt(j) - 'a']++;
-            }
+        int k = p.length();
+        // first window
+        for (int i = 0; i < k; i++) {
+            sCount[s.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(pCount, sCount)) result.add(0);
+
+        // slide window
+        for (int i = k; i < s.length(); i++) {
+            sCount[s.charAt(i) - 'a']++;              // add new char
+            sCount[s.charAt(i - k) - 'a']--;          // remove old char
             if (Arrays.equals(pCount, sCount)) {
-                result.add(i);
+                result.add(i - k + 1);
             }
         }
         return result;
