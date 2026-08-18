a11628ffd1346bfb82a458d423635f5a08c4d395
Pushkala-22 <pushkalachelluri@gmail.com>
2026-08-18 09:48:09 +0530

    Time: 15 ms (59.59%) | Memory: 51.2 MB (88.22%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,18 +1,13 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
         int gl=g.length;
-        int sl=s.length;
       Arrays.sort(g);
       Arrays.sort(s);
       int c=0;
-      for(int i=0;i<gl;i++){
-        for(int j=0;j<sl;j++){
-            if(g[i]<=s[j]){
-                c++;
-                s[j]=-1;
-                break;
-            }
-        }
+      for(int i:s){
+       if(c<gl && i>=g[c]){
+          c++;
+       }
       }
       return c;
     }
